329ed6612509e3a554148f716fe7a298acad1f92
Anton <AimII@yandex.ru>
2014-11-26 17:13:31 +0300

    [TEST] Moving functionality
    Перенес библиотеку луа функций в отдельный файл, упаковал процесс
    инициализации и загрузки луа-машины в функции. Наметил функции
    управления выполнением луа-скриптов.
    Обновление ни разу не компилировалось, может быть неработоспособным!

--- Assets/ProcLibs.cs
+++ Assets/ProcLibs.cs
@@ -3,6 +3,153 @@ using System.Collections;
 using UniLua;
 
 public class ProcLibs : MonoBehaviour {
+	 Texture2D Scr = this.GetComponent<processor>().Scr;
+	
+	void Start() {
+		
+	}
+	
+	public int OpenIOLib(ILuaState lua)
+	{
+		var define = new NameFuncPair[] // структура, описывающая все доступные методы (интерфейс Lua -> C#)
+		{
+			new NameFuncPair("trace", L_Trace),
+			new NameFuncPair("wait", L_Wait),
+			new NameFuncPair("pause", L_Pause),
+			new NameFuncPair("getTime", L_GetTime),
+			new NameFuncPair("getDeltaTime", L_GetDeltaTime),
+			new NameFuncPair("getBrightness", L_GetBrightness),
+			new NameFuncPair("getXPos", L_GetXPos),
+			new NameFuncPair("getYPos", L_GetYPos),
+			new NameFuncPair("getZPos", L_GetZPos),
+			new NameFuncPair("getPitch", L_GetPitch),
+			new NameFuncPair("getYaw", L_GetYaw),
+			new NameFuncPair("getRoll", L_GetRoll),
+			new NameFuncPair("move", L_Move),
+		};
+
+		lua.L_NewLib(define);
+		return 1;
+	}
+	
+	public int OpenGraphicsLib(ILuaState lua)
+	{
+		var define = new NameFuncPair[] // структура, описывающая все доступные методы (интерфейс Lua -> C#)
+		{
+			new NameFuncPair("drawPixel", L_SetPixel),
+			new NameFuncPair("drawColor", L_SetColor),
+			new NameFuncPair("drawDot", L_SetPixelCol),
+			new NameFuncPair("redraw", L_Redraw), 
+			new NameFuncPair("clrscr", L_ClearScreen),
+			new NameFuncPair("drawLine", L_DrawLine),
+		};
+
+		lua.L_NewLib(define);
+		return 1;
+	}
+	
+	
+
+	private int L_Trace(ILuaState s)
+	{
+		Debug.Log("Lua trace: " + s.L_CheckString(1)+"Time:"+ Time.time.ToString()); // читаем первый параметр
+		return 1; // так надо
+	}
+
+	private int L_Wait(ILuaState s)
+	{
+
+		waitTime =(float) s.L_CheckNumber (1);
+		waitFlag = true;
+		waitStartTime = Time.time;
+
+		_temp_state = null;
+		_temp_state = s;
+
+		return s.YieldK (s.GetTop(), 0, null);
+	}
+
+	private int L_Pause(ILuaState s)
+	{
+		_temp_state = null;
+		_temp_state = s;
+		return s.YieldK (s.GetTop(), 0, null);
+
+	}
+
+	public int L_SetPixelCol(ILuaState s)
+	{
+		int x = (int) s.L_CheckNumber (1);
+		int y = (int) s.L_CheckNumber (2);
+		float R = (float) s.L_CheckNumber (3);
+		float G = (float) s.L_CheckNumber (4);
+		float B = (float) s.L_CheckNumber (5);
+		Scr.SetPixel (x, y, new Color (R, G, B));
+		return 1;
+	}
+
+	public int L_SetPixel(ILuaState s)
+	{
+		int x = (int) s.L_CheckNumber (1);
+		int y = (int) s.L_CheckNumber (2);
+		Scr.SetPixel (x, y, drawCol);
+		Scr.Apply ();
+		return 1;
+	}
+
+	public int L_SetColor(ILuaState s)
+	{
+		float R = (float) s.L_CheckNumber (1);
+		float G = (float) s.L_CheckNumber (2);
+		float B = (float) s.L_CheckNumber (3);
+		drawCol = new Color (R, G, B);
+		return 1;
+	}
+
+	public int L_Redraw(ILuaState s)
+	{
+		Scr.Apply ();
+		return 1;
+	}
+
+	public int L_DrawLine(ILuaState s)
+	{
+	int x0 = s.L_CheckNumber(1);
+	int y0 = s.L_CheckNumber(2);
+	int x1 = s.L_CheckNumber(3);
+	int y1 = s.L_CheckNumber(4);
+	
+	
+	
+	return 1;
+	}
+	
+	public int L_ClearScreen(ILuaState s)
+	{
+	//Scr.SetPixels( Надо дописать, автокомплита нет.
+	Scr.Apply();
+	return 1;
+	}
+	
+	
+	
+	public int L_GetBrightness(ILuaState s)
+	{
+		int index = (int)s.L_CheckNumber(1);
+		float br = 0f;
+		//Sensors [index - 1].SendMessage ("GetBrightness");
+		ColSensor Col = Sensors [index - 1].GetComponent<ColSensor> ();
+		if (!Col) {
+			br = Sensors [index - 1].GetComponent<CamSensor> ().GetBrightness ();
+
+		} else {
+			br = Col.GetBrightness ();
+
+		}
+		s.PushNumber ((double)br);
+		return 1;
+	}
+
 
 	public int L_GetTime(ILuaState s)
 	{
@@ -10,7 +157,67 @@ public class ProcLibs : MonoBehaviour {
 		return 1;
 	}
 
-	
+	private int L_Move(ILuaState s)
+	{
+		var index = s.L_CheckNumber(1);
+		var dir = s.L_CheckNumber(2);
+		var speed = s.L_CheckNumber(3);
+
+		JointMotor motor = Motors[(int)index-1].hingeJoint.motor;
+		motor.targetVelocity = (float)speed*(float)dir;
+		Motors[(int)index - 1].hingeJoint.motor = motor;
+
+		return 1; // так надо
+	}
+
+	private int L_GetDeltaTime(ILuaState s)
+	{
+
+		s.PushNumber(Time.deltaTime);
+		return 1; 
+	}
+
+	private int L_GetXPos(ILuaState s)
+	{
+
+		s.PushNumber(gameObject.transform.position.x);
+		return 1; 
+	}
+
+	private int L_GetYPos(ILuaState s)
+	{
+
+		s.PushNumber(gameObject.transform.position.y);
+		return 1; 
+	}
+
+	private int L_GetZPos(ILuaState s)
+	{
+
+		s.PushNumber(gameObject.transform.position.z);
+		return 1; 
+	}
+
+	private int L_GetPitch(ILuaState s)
+	{
+
+		s.PushNumber(gameObject.transform.rotation.eulerAngles.x);
+		return 1; 
+	}
+
+	private int L_GetYaw(ILuaState s)
+	{
+
+		s.PushNumber(gameObject.transform.rotation.eulerAngles.y);
+		return 1; 
+	}
+
+	private int L_GetRoll(ILuaState s)
+	{
+
+		s.PushNumber(gameObject.transform.rotation.eulerAngles.z);
+		return 1; 
+	}
 
 
 
--- Assets/processor.cs
+++ Assets/processor.cs
@@ -11,8 +11,10 @@ public class processor : MonoBehaviour {
 	public Texture2D Scr;
 	private Color drawCol;
 
+	
 	public string scriptPath;
-
+	public bool isRunning;
+	
 	private ILuaState _lua; // через этот объект будет производится работа с Lua
 	private ThreadStatus _status; // объект для работы с конкретным скриптом
 	private ILuaState _temp_state;
@@ -22,32 +24,29 @@ public class processor : MonoBehaviour {
 	private float waitStartTime;
 	private float waitTime;
 
-	private int pauseCnt = 0;
 
 	void Awake () {
-
-		_lua = LuaAPI.NewState();			
-		_lua.L_OpenLibs();
-		_lua.L_RequireF("io", OpenLib, true);
-		var _thread = _lua.NewThread();
-		_status = _lua.L_LoadFile (scriptPath);
-		Debug.Log (_status);
-		//_lua.L_DoFile (scriptPath);
-		_lua.Resume(null, 0);
-		//_temp_state.Resume (null, 0);
+	
 	}
 
 	void Start () {
 		Scr = new Texture2D (128, 128, TextureFormat.RGB24, false);
-
+		
+		LUA_Init();
+		//LUA_LoadAndRun(scriptPath);
+	
 	}
 	
 
 	void Update () {
 	
-		if (Input.GetKeyDown ("a")) {
+		if (Input.GetKeyDown ("p")) {
 			if (_temp_state.GetTop() > 0) _lua.Resume(null, 0);
 			_lua.Resume(null, 0);
+		}
+		
+		if (Input.GetKeyDown ("r")) { //Пусть пока запуск будет по нажатию кнопки
+			LUA_LoadAndRun(scriptPath);
 		}
 
 		if (waitFlag) {
@@ -56,32 +55,34 @@ public class processor : MonoBehaviour {
 
 		}
 	}
-
-	private int OpenLib(ILuaState lua)
-	{
-		var define = new NameFuncPair[] // структура, описывающая все доступные методы (интерфейс Lua -> C#)
-		{
-			new NameFuncPair("trace", L_Trace),
-			new NameFuncPair("wait", L_Wait),
-			new NameFuncPair("pause", L_Pause),
-			new NameFuncPair("getTime", L_GetTime),
-			new NameFuncPair("getDeltaTime", L_GetDeltaTime),
-			new NameFuncPair("getBrightness", L_GetBrightness),
-			new NameFuncPair("getXPos", L_GetXPos),
-			new NameFuncPair("getYPos", L_GetYPos),
-			new NameFuncPair("getZPos", L_GetZPos),
-			new NameFuncPair("getPitch", L_GetPitch),
-			new NameFuncPair("getYaw", L_GetYaw),
-			new NameFuncPair("getRoll", L_GetRoll),
-			new NameFuncPair("move", L_Move),
-			new NameFuncPair("drawPixel", L_SetPixel),
-			new NameFuncPair("drawColor", L_SetColor),
-			new NameFuncPair("drawDot", L_SetPixelCol),
-			new NameFuncPair("redraw", L_Redraw),
-		};
-
-		lua.L_NewLib(define);
-		return 1;
+	
+	private bool LUA_Init() {
+		_lua = LuaAPI.NewState();			
+		_lua.L_OpenLibs();
+		_lua.L_RequireF("io", ProcLibs.OpenIOLib, true);
+		_lua.L_RequireF("graph", this.GetComponent<ProcLibs>().OpenGraphicsLib, true);
+		var _thread = _lua.NewThread();
+		return true;
+	}
+	
+	private bool LUA_LoadAndRun(string path) {
+		_status = _lua.L_LoadFile (path);
+		if(_status == ThreadStatus.LUA_OK) {
+			_lua.Resume(null, 0);
+			isRunning = true;
+			return true;
+		} else {
+			Debug.Log("Failed to run lua script.");
+			Debug.Log (_status);
+			isRunning = false;
+			return false;
+		}
+	}
+	
+	private bool LUA_Stop() {
+	
+	isRunning = false;
+	return true;
 	}
 
 	private void CheckWait()
@@ -94,155 +95,6 @@ public class processor : MonoBehaviour {
 				waitFlag = false;
 			} 
 		}
-	}
-
-
-	private int L_Trace(ILuaState s)
-	{
-		Debug.Log("Lua trace: " + s.L_CheckString(1)+"Time:"+ Time.time.ToString()); // читаем первый параметр
-		return 1; // так надо
-	}
-
-	private int L_Wait(ILuaState s)
-	{
-
-		waitTime =(float) s.L_CheckNumber (1);
-		waitFlag = true;
-		waitStartTime = Time.time;
-
-		_temp_state = null;
-		_temp_state = s;
-
-		return s.YieldK (s.GetTop(), 0, null);
-	}
-
-	private int L_Pause(ILuaState s)
-	{
-		_temp_state = null;
-		_temp_state = s;
-		return s.YieldK (s.GetTop(), 0, null);
-
-	}
-
-	public int L_SetPixelCol(ILuaState s)
-	{
-		int x = (int) s.L_CheckNumber (1);
-		int y = (int) s.L_CheckNumber (2);
-		float R = (float) s.L_CheckNumber (3);
-		float G = (float) s.L_CheckNumber (4);
-		float B = (float) s.L_CheckNumber (5);
-		Scr.SetPixel (x, y, new Color (R, G, B));
-		return 1;
-	}
-
-	public int L_SetPixel(ILuaState s)
-	{
-		int x = (int) s.L_CheckNumber (1);
-		int y = (int) s.L_CheckNumber (2);
-		Scr.SetPixel (x, y, drawCol);
-		Scr.Apply ();
-		return 1;
-	}
-
-	public int L_SetColor(ILuaState s)
-	{
-		float R = (float) s.L_CheckNumber (1);
-		float G = (float) s.L_CheckNumber (2);
-		float B = (float) s.L_CheckNumber (3);
-		drawCol = new Color (R, G, B);
-		return 1;
-	}
-
-	public int L_Redraw(ILuaState s)
-	{
-		Scr.Apply ();
-		return 1;
-	}
-
-	public int L_GetBrightness(ILuaState s)
-	{
-		int index = (int)s.L_CheckNumber(1);
-		float br = 0f;
-		//Sensors [index - 1].SendMessage ("GetBrightness");
-		ColSensor Col = Sensors [index - 1].GetComponent<ColSensor> ();
-		if (!Col) {
-			br = Sensors [index - 1].GetComponent<CamSensor> ().GetBrightness ();
-
-		} else {
-			br = Col.GetBrightness ();
-
-		}
-		s.PushNumber ((double)br);
-		return 1;
-	}
-
-
-	public int L_GetTime(ILuaState s)
-	{
-		s.PushNumber ((double)Time.time);
-		return 1;
-	}
-
-	private int L_Move(ILuaState s)
-	{
-		var index = s.L_CheckNumber(1);
-		var dir = s.L_CheckNumber(2);
-		var speed = s.L_CheckNumber(3);
-
-		JointMotor motor = Motors[(int)index-1].hingeJoint.motor;
-		motor.targetVelocity = (float)speed*(float)dir;
-		Motors[(int)index - 1].hingeJoint.motor = motor;
-
-		return 1; // так надо
-	}
-
-	private int L_GetDeltaTime(ILuaState s)
-	{
-
-		s.PushNumber(Time.deltaTime);
-		return 1; // так надо
-	}
-
-	private int L_GetXPos(ILuaState s)
-	{
-
-		s.PushNumber(gameObject.transform.position.x);
-		return 1; // так надо
-	}
-
-	private int L_GetYPos(ILuaState s)
-	{
-
-		s.PushNumber(gameObject.transform.position.y);
-		return 1; // так надо
-	}
-
-	private int L_GetZPos(ILuaState s)
-	{
-
-		s.PushNumber(gameObject.transform.position.z);
-		return 1; // так надо
-	}
-
-	private int L_GetPitch(ILuaState s)
-	{
-
-		s.PushNumber(gameObject.transform.rotation.eulerAngles.x);
-		return 1; // так надо
-	}
-
-	private int L_GetYaw(ILuaState s)
-	{
-
-		s.PushNumber(gameObject.transform.rotation.eulerAngles.y);
-		return 1; // так надо
-	}
-
-	private int L_GetRoll(ILuaState s)
-	{
-
-		s.PushNumber(gameObject.transform.rotation.eulerAngles.z);
-		return 1; // так надо
 	}
 
 }
